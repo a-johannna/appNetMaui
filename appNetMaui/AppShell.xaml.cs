a1a85817352ca883df3a8b75a8767183a8cfe7ec
andrea.villavicenc <andrea.villavicenc@5300101A107016>
2025-01-22 19:31:19 +0100

    Agregar archivos de proyecto.

---
+++ appNetMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace appNetMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ appNetMaui/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace appNetMaui   
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        private bool _isHovering = false;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count += 5 ;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+
+        private void OnImageTapped(object sender, EventArgs e)
+        {
+            if (_isHovering)
+            {
+                MyImage.Source = "jeff_marvel.png"; // Imagen original
+            }
+            else
+            {
+                MyImage.Source = "jeff_dolphin.png"; // Imagen de cambio
+            }
+
+            _isHovering = !_isHovering; // Cambia el estado
+        }
+    }
+
+}
